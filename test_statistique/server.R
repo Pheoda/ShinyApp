bc7e2f8e4095b37f499f40b176575dfbd721690e
Pheoda <n.fondacci@gmail.com>
2017-05-10 20:50:19 +0200

    Traitement du fichier excel en le convertissant en matrice colonne

--- test_statistique/server.R
+++ test_statistique/server.R
@@ -2,11 +2,13 @@ library(shiny)
 library(readxl)
 
 shinyServer(function(input, output) {
-
-  output$text <- renderText({
-    if(is.null(input$file1) || is.null(input$file2))
-      return(NULL);
-    var.test(read_excel(input$file1, col_names = FALSE), read_excel(input$file2, col_names = FALSE));
+  
+  output$text <- renderPrint({
+    if(!is.null(input$file1) && !is.null(input$file2))
+    {
+      x <- c(as.matrix(read_excel(input$file1, col_names = FALSE)));
+      y <- c(as.matrix(read_excel(input$file2, col_names = FALSE)));
+    }
   })
   
 })
