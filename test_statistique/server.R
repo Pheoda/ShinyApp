d91fb3f2d670e0fb234ff05aee983203fec836d7
Pheoda <n.fondacci@gmail.com>
2017-06-08 19:39:18 +0200

    Ajout test KS

--- test_statistique/server.R
+++ test_statistique/server.R
@@ -2,6 +2,10 @@ library(shiny)
 
 shinyServer(function(input, output) {
   
+  ######
+  ### Comparaison de 2 variances
+  ######
+  
   output$p_value <- renderText({
     #Vérification si des fichiers de données on été rentrés
     if(!is.null(input$file1) && !is.null(input$file2))
@@ -33,6 +37,45 @@ shinyServer(function(input, output) {
       plot(abscisse, x, main = "Rendu des échantillons", xlab = "Numéro", ylab = "Valeur", type = "p");
       #Affichage du second jeu en rouge
       points(abscisse, y, col="red");
+    }
+  })
+  
+  
+  ######
+  ### Test de Kolmogorov Smirnov
+  ######
+  
+  output$p_valueKS <- renderText({
+    #Vérification si des fichiers de données on été rentrés
+    if(!is.null(input$file1KS) && !is.null(input$file2KS))
+    {
+      #Rangement des valeurs dans des tableaux
+      xKS <- c(as.matrix(read.csv(input$file1KS$datapath)));
+      yKS <- c(as.matrix(read.csv(input$file2KS$datapath)));
+      
+      #Affichage de la p-value retournée
+      paste("Valeur de p : ", ks.test(xKS, yKS)$p.value);
+    }
+  })
+  
+  output$plotKS <- renderPlot({
+    #Vérification si des fichiers de données on été rentrés
+    if(!is.null(input$file1KS) && !is.null(input$file2KS))
+    {
+      #Rangement des valeurs dans des tableaux
+      xKS <- c(as.matrix(read.csv(input$file1KS$datapath)));
+      yKS <- c(as.matrix(read.csv(input$file2KS$datapath)));
+      
+      #Tri par ordre croissant des valeurs reçues
+      xKS <- sort(xKS);
+      yKS <- sort(yKS);
+      
+      abscisseKS <- c(1:length(xKS));
+      
+      #Affichage du premier jeu en noir
+      plot(abscisseKS, xKS, main = "Rendu des échantillons", xlab = "Numéro", ylab = "Valeur", type = "p");
+      #Affichage du second jeu en rouge
+      points(abscisseKS, yKS, col="red");
     }
   })
   
