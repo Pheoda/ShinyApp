4549c709dc9ca6a1cf6e805b26d3aa5f41d975ad
Pheoda <n.fondacci@gmail.com>
2017-06-08 19:33:37 +0200

    Ajout KS ui.r

--- test_statistique/ui.R
+++ test_statistique/ui.R
@@ -16,5 +16,21 @@ shinyUI(fluidPage(
       textOutput("p_value"), #Affichage du texte de la p-value
       plotOutput("plot")     #Affichage du graphique
     )
+  ),
+  
+  titlePanel("Test de Kolmogorov Smirnov"),
+  
+  # Entrée des 2 fichiers sources
+  sidebarLayout(
+    sidebarPanel(
+      fileInput("file1KS", "Fichier échantillon 1", accept=c('text/csv', 'text/comma-separated-values,text/plain', '.csv')),
+      fileInput("file2KS", "Fichier échantillon 2", accept=c('text/csv', 'text/comma-separated-values,text/plain', '.csv'))
+    ),
+    
+    # Affichage du rendu
+    mainPanel(
+      textOutput("p_valueKS"), #Affichage du texte de la p-value
+      plotOutput("plotKS")     #Affichage du graphique
+    )
   )
 ))
